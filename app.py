9000d82b000cc4abf096071b1a3a0c2e973d238f
Sondre Haugom <sondrehaugom@gmail.com>
2026-09-21 09:27:51 +0200

    Rettet i hvilke kolonne jeg skal bruke vidre og filtrert vekk resten :)

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ def get_data():
     try:
         data = pd.read_csv("qa 2.csv")
 
-        df_filtered = data.drop(columns=["id", "datetime", "title", "bot_response"])
+        df_filtered = data.drop(columns=["id", "title", "bot_response", "user_question"])
 
         df_filtered.to_csv("filtered_data.csv",  index=False)
 
     except FileExistsError as e:
         print("Can`t find file!", e)
+
+
+
 
 get_data()
